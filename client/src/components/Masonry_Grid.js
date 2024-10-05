60304f69baf9b7becbf76e9c4a142817eb90a29d
SomeOneWho <105697151+MahatVasudev@users.noreply.github.com>
2024-10-05 22:59:22 +0530

    New component
    Creating a new component for Masonry grid...
    should be updated later to accomadate async loading of images

---
+++ client/src/components/Masonry_Grid.js
@@ -0,0 +1,32 @@
+
+import React from 'react';
+
+import Masonry from "react-masonry-css";
+
+
+const MasonryGrid = ({ data }) => {
+  const breakpointColumnsObj = {
+    default: 6,  // 4 columns by default
+    1800: 5,
+    1300: 4,
+    1100: 3,     // 3 columns for screens <= 1100px
+    700: 2,      // 2 columns for screens <= 700px
+  };
+
+  return (
+
+    <Masonry
+      breakpointCols={breakpointColumnsObj}
+      className="my-masonry-grid"
+      columnClassName="my-masonry-grid_column"
+    >
+      {data.map((src, index) => (
+        <div key={index} className="mb-4">
+          <img src={src} alt={`Image ${index + 1}`} className="w-full h-auto rounded-lg shadow-md" />
+        </div>
+      ))}
+    </Masonry>
+  );
+};
+
+export default MasonryGrid;
